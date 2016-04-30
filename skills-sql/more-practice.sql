a4cb6eadc55f172a7803d4b23c51cc7279e0dd0f
munnu <moniqueblake4@gmail.com>
2016-04-30 13:02:30 -0700

    Completed the more-practice.sql

--- skills-sql/more-practice.sql
+++ skills-sql/more-practice.sql
@@ -3,9 +3,16 @@
 
 
 -- INSERT
-
+insert into models (year, brand_name, name) values (2015, 'Chevorlet', 'Malibu');
+insert into models (year, brand_name, name) values (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
-
+create table Awards (
+    name varchar(30),
+    year int,
+    winner_id int
+);
 
 -- More INSERT
+insert into Awards values ('IIHS Safety Award', 2015, 49);
+insert into Awards values ('IIHS Safety Award', 2015, 50);
